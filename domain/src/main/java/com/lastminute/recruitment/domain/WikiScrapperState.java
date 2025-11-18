6312fb2415a19783abdfb1f090ccea8d779022ef
Lzgpom <lmgpom@gmail.com>
2025-11-18 22:04:40 +0000

    feat: changed processed link implementation from a hash set to a bloom filter.

--- domain/src/main/java/com/lastminute/recruitment/domain/WikiScrapperState.java
+++ domain/src/main/java/com/lastminute/recruitment/domain/WikiScrapperState.java
@@ -1,18 +1,25 @@
 package com.lastminute.recruitment.domain;
 
-import java.util.HashSet;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 
 public class WikiScrapperState {
+    private static final int EXPECTED_NUMBER_OF_ELEMENTS = 5;
 
-    private final Set<String> processedLinks;
+    private final BloomFilter<String> processedLinks;
     private final Queue<String> linksToProcess;
 
     public WikiScrapperState() {
-        this.processedLinks = new HashSet<>();
+        this.processedLinks = BloomFilter.create(
+                Funnels.stringFunnel(StandardCharsets.UTF_8),
+                EXPECTED_NUMBER_OF_ELEMENTS
+        );
+
         this.linksToProcess = new LinkedList<>();
     }
 
@@ -24,13 +31,13 @@ public class WikiScrapperState {
 
     public void addLinkToProcess(final String link) {
         if (!isLinkProcessed(link)) {
-            processedLinks.add(link);
+            processedLinks.put(link);
             linksToProcess.add(link);
         }
     }
 
     private boolean isLinkProcessed(final String link) {
-        return processedLinks.contains(link);
+        return processedLinks.mightContain(link);
     }
 
     public String pollLink() {
